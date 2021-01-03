2a03fc0da0f4daff935a52d057c65a701266e8b2
Eneki <joanreneki@gmail.com>
2021-01-04 00:37:46 +0300

    Format

--- models/initiative.js
+++ models/initiative.js
@@ -3,34 +3,34 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const initiativeSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    leadName: {
-        type: String,
-        required: true
-    },
-    leadEmail: {
-        type: String,
-        required: true
-    },
-    startYear: {
-        type: String,
-        required: true
-    },
-    endYear: {
-        type: String,
-        required: true
-    },
-    statement: {
-        type: String,
-        required: true
-    },
-    goalTeam: {
-        type: String,
-        required: true
-    }
-})
+	name: {
+		type: String,
+		required: true,
+	},
+	leadName: {
+		type: String,
+		required: true,
+	},
+	leadEmail: {
+		type: String,
+		required: true,
+	},
+	startYear: {
+		type: String,
+		required: true,
+	},
+	endYear: {
+		type: String,
+		required: true,
+	},
+	initiativeName: {
+		type: String,
+		required: true,
+	},
+	goalTeam: {
+		type: String,
+		required: true,
+	},
+});
 
 module.exports = mongoose.model('Initiative', initiativeSchema);
